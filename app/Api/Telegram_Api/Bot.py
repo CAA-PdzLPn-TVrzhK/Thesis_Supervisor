0adb53b0c63d2981dd757eb2925eeb7acce608c1
tdzdslippen <ars.laptev@innopolis.university>
2025-06-06 17:02:24 +0300

    with simple authorization, without db

--- app/Api/Telegram_Api/Bot.py
+++ app/Api/Telegram_Api/Bot.py
@@ -1,18 +1,23 @@
 import os
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.storage.memory import MemoryStorage
 
 API_TOKEN = "7766131056:AAF70m3Omm0BeaXbRSOm_pzIQCtbPckzBCA"
 
-email_bd: set[str] = set()
-email_bd.add("ars.laptev@innopolis.university")
-email_bd.add("d.khasanshin@innopolis.university")
+email_bd: set[str] = {
+    "ars.laptev@innopolis.university",
+    "d.khasanshin@innopolis.university",
+    "ru.nasibullin@innopolis.university",
+    "n.selezenev@innopolis.university",
+    "t.kagarmanov@innopolis.university"
+}
 
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(storage=MemoryStorage())
 
 class Form(StatesGroup):
     waiting_for_text = State()
@@ -25,8 +30,19 @@ async def cmd_start(message: Message, state: FSMContext):
 @dp.message(Form.waiting_for_text)
 async def process_next_message(message: Message, state: FSMContext):
     user_email = message.text
-    if user_email in
-    await bot.send_message(message.from_user.id, f"{user_text}")
+    if user_email in email_bd:
+        keyboard = ReplyKeyboardMarkup(
+            keyboard=[
+                [KeyboardButton(text="Submit work"), KeyboardButton(text="Пойти нахуй")]
+            ],
+            resize_keyboard=True
+        )
+        await message.answer(
+            "You are logged in",
+            reply_markup=keyboard
+        )
+    else:
+        await message.answer("You are not allowed to send messages", reply_markup=ReplyKeyboardMarkup(keyboard=[]))
     await state.clear()
 
 if __name__ == "__main__":
