6db8c30aaf1a807b833b91187c54fbfc645a440b
shokoladniyVzhuh <n.selezenev@mail.ru>
2025-06-15 02:25:31 +0300

    verificator

--- app/front/App.jsx
+++ app/front/App.jsx
@@ -1,2 +1,12 @@
-import main_page from "./components/main_page"
-import student_list from "./components/main_page"
+import MainPage from "./components/main_page"
+import student_list from "./components/student_list"
+
+function App() {
+    return (
+    <div>
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
