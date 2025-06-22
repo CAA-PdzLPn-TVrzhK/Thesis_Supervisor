981eb74b440bfd3245def790ad38131e7f45be03
Ruslan <ru.nasibullin@innopolis.university>
2025-06-23 01:15:33 +0300

    fix

--- app/Presentation/tg_mini_app/src/App.jsx
+++ app/Presentation/tg_mini_app/src/App.jsx
@@ -30,7 +30,7 @@ class App extends React.Component{
     }
 
     render() {
-        if (this.state.data.role === "student") {
+        if (this.state.data.role == "student") {
             return (
                 <div>
                     <StudentProfile data={this.state.data}/>
