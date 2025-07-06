ac36ee01db6f0b966f7bc4d204627ea23a905e00
Timur <timyr20060430@gmail.com>
2025-07-06 16:32:53 +0300

    add empty cells in student profile if group or supervisor is empty

--- app/Presentation/tg_mini_app/src/Student/PagesContent/ProfileElements/getSupervisorData.jsx
+++ app/Presentation/tg_mini_app/src/Student/PagesContent/ProfileElements/getSupervisorData.jsx
@@ -1,10 +1,16 @@
 import axios from "axios";
 
 export async function getSupervisorData(supervisorId) {
-    const supervisorDate = await axios.get(`${window.TelegramWebApp.API_BASE}supervisors?id=eq.${supervisorId}`,
+    try {
+        const supervisorDate = await axios.get(`${window.TelegramWebApp.API_BASE}supervisors?id=eq.${supervisorId}`,
         {
             headers: window.TelegramWebApp.headers,
-        }
-    );
-    return supervisorDate.data;
+            }
+        );
+        return supervisorDate.data;
+    } catch (e) {
+        console.log("error in get supervisor data", e);
+        return [];
+    }
+
 }
