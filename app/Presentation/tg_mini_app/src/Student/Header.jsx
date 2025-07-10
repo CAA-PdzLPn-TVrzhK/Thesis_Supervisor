6e1af5e73d8d33c2a71620fef0cee6485e733385
Timur <timyr20060430@gmail.com>
2025-07-11 00:27:02 +0300

    add settings into header

--- app/Presentation/tg_mini_app/src/Student/Header.jsx
+++ app/Presentation/tg_mini_app/src/Student/Header.jsx
@@ -2,20 +2,40 @@
 import React from "react"
 import "./BasePageComponent.css"
 import {IconSchool} from "@tabler/icons-react";
+import Settings from "./Settings/Settings.jsx";
+import {IconSettings} from "@tabler/icons-react";
 
 class Header extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            settings: false,
+        }
+        this.changeSettings = this.changeSettings.bind(this);
+    }
+
+    changeSettings() {
+        this.setState((p) => {
+            return {
+                settings: !p.settings,
+            }
+        })
+    }
+
     render() {
         return (
+            <>
             <div className="header header-top">
                 <div className="header-logo-block">
                     <IconSchool size={36} className="header-logo"/>
                     <span className="header-title">Innopolis Thesis Supervisor</span>
                 </div>
-                <div className="header-help-block">
-                    <span className="header-help-email">innopolis.university@gmail.com</span>
-                    <span className="header-help-phone">8-800-555-35-35</span>
+                <div className="header-settings-block">
+                    <IconSettings size={24} className="header-settings-icon" onClick={() => this.changeSettings()}/>
                 </div>
             </div>
+            {this.state.settings ? <Settings close = {this.changeSettings} /> : ""}
+            </>
         )
     }
 }
---
+++ app/Presentation/tg_mini_app/src/Student/Settings/Settings.jsx
@@ -0,0 +1,49 @@
+
+import React from "react"
+import { IconEdit, IconX } from "@tabler/icons-react";
+import "./Settings.css"
+import ReactDOM from "react-dom";
+
+class Settings extends React.Component {
+    constructor(props) {
+        super(props);
+        this.close = this.close.bind(this);
+    }
+
+    close() {
+        this.props.close();
+    }
+
+    render() {
+        return ReactDOM.createPortal (
+            (<div className="settings-overlay">
+                <div className="settings-modal">
+                    <div className="settings-header">
+                        <h2 className="settings-title">Settings</h2>
+                        <IconX size={20} className="settings-close-icon" onClick={this.close}/>
+                    </div>
+                    
+                    <div className="settings-content">
+                        <div className="settings-section">
+                            <h3 className="settings-section-title">Profile</h3>
+                            <div className="settings-item">
+                                <span className="settings-item-label">Change Profile Info</span>
+                                <IconEdit size={18} className="settings-item-icon"onClick={() => handleEdit()} />
+                            </div>
+                        </div>
+                        
+                        <div className="settings-section">
+                            <h3 className="settings-section-title">Contact Support</h3>
+                            <div className="settings-item">
+                                <div className="settings-item-email">innopolis.university@gmail.com</div>
+                                <div className="settings-item-phone">8-800-555-35-35</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>), document.getElementById("modal-root")
+        )
+    }
+}
+
+export default Settings
