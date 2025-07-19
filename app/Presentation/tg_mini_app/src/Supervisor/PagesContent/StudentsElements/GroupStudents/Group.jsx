299109f5d6e1a7ce71f0eb6688c69257d045d041
Timur <timyr20060430@gmail.com>
2025-07-20 00:30:22 +0300

    add list of students

--- app/Presentation/tg_mini_app/src/Supervisor/PagesContent/StudentsElements/GroupStudents/Group.jsx
+++ app/Presentation/tg_mini_app/src/Supervisor/PagesContent/StudentsElements/GroupStudents/Group.jsx
@@ -1,6 +1,45 @@
 
-import React from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios";
 
-export function getGroup() {
-    
+
+
+export default function GetGroup({group}) {
+    const [open, setOpen] = useState(false);
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        const getStudents = async () => {
+            const students = await axios.get(`${window.TelegramWebApp.API_BASE}students?peer_group_id=eq.${group.id}`, {headers: window.TelegramWebApp.headers});
+            const info = [];
+
+            for(let student of students.data) {
+                const user = await axios.get(`${window.TelegramWebApp.API_BASE}users?id=eq.${student.user_id}`, {headers: window.TelegramWebApp.headers});
+                info.push({user: user.data[0], student: student});
+            }
+            setStudents(info);
+        };
+        getStudents();
+    }, [group]);
+
+    return(
+           <div>
+               <div onClick={() => setOpen(!open)}>
+                   {group.name}
+               </div>
+               {open && (
+                    <div>
+                        {students.map((studentData) => {
+                            return (
+                                <div key={studentData.user.id}>
+                                    <div>
+                                        <div> {studentData.user.first_name} {studentData.user.last_name} </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                   </div>
+               )}
+           </div>
+    )
 }
--- app/Presentation/tg_mini_app/src/Supervisor/PagesContent/StudentsElements/Students.jsx
+++ app/Presentation/tg_mini_app/src/Supervisor/PagesContent/StudentsElements/Students.jsx
@@ -1,17 +1,39 @@
 
 import React from "react"
+import axios from "axios";
+import GetGroup from "./GroupStudents/Group.jsx";
 
 class Students extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            groups: null,
+        }
+    }
+
+    async componentDidMount() {
+        const supervisor = await axios.get(`${window.TelegramWebApp.API_BASE}supervisors?user_id=eq.${window.TelegramWebApp.userId}`, {headers: window.TelegramWebApp.headers});
+        const groups = await axios.get(`${window.TelegramWebApp.API_BASE}peer_groups?supervisor_id=eq.${supervisor.data[0].id}`, {headers: window.TelegramWebApp.headers});
+        this.setState({groups: groups.data});
     }
 
     render() {
+        if(this.state.groups === null) {
+            return (
+                <div className = "loader-container">
+                    <img className = "loader-image" src="https://megakeys.info/icons/loader.gif" alt="Please, wait a bit" />
+                </div>
+            )
+        }
         return (
             <div>
                 <div> Group list </div>
                 <div>
-
+                    {this.state.groups.map((group) => {
+                        return (
+                            <GetGroup group={group} key={group.id}/>
+                        )
+                    })}
                 </div>
             </div>
         )
